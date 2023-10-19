404bc1512bb2a4c97770a6019c1b38b07496dfb9
Ismail Bajrami <ismo.bjr@gmail.com>
2023-10-19 17:08:09 +0200

    searchbar

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header/Header';
 import BannerSponsor from './components/BannerSponsors/BannerSponsor';
+import CardsList from './components/CardsContainer/CardsList/CardsList';
 function App() {
   return (
     <div className="App">
       <Header />
       <BannerSponsor />
+      <CardsList />
     </div>
   );
 }
---
+++ app/src/components/CardsContainer/CardsList/CardsList.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react"
+import style from "./style.module.css"
+import hotIcon from "../../../assets/icons/hot-icon.svg"
+import newIcon from "../../../assets/icons/new.svg"
+import premiumIcon from "../../../assets/icons/premium.svg"
+import searchIcon from "../../../assets/icons/search-icon.svg"
+
+const CardsList = () => {
+	const [selectedItem, setSelectedItem] = useState(null)
+
+	const handleItemClick = (index) => {
+		setSelectedItem(index)
+	}
+
+	return (
+		<span className={style["filter-container"]}>
+			<ul className={style.filter}>
+				<li
+					className={`${selectedItem === 0 ? `${style.selected} ` : ""}`}
+					onClick={() => handleItemClick(0)}>
+					<img
+						src={hotIcon}
+						alt=""
+						height={25}
+					/>
+					Hot
+				</li>
+
+				<li
+					className={`${selectedItem === 2 ? `${style.selected} ` : ""}`}
+					onClick={() => handleItemClick(2)}>
+					<img
+						src={premiumIcon}
+						alt=""
+						height={25}
+					/>
+					Premium
+				</li>
+				<li
+					className={`${selectedItem === 1 ? `${style.selected} ` : ""}`}
+					onClick={() => handleItemClick(1)}>
+					<img
+						src={newIcon}
+						alt=""
+						height={25}
+					/>
+					New
+				</li>
+				<span className={style["search-bar"]}>
+					<div className="icon">
+						{" "}
+						<img
+							src={searchIcon}
+							alt=""
+							height={20}
+						/>
+					</div>
+					<input
+						type="text"
+						name="Search a Box"
+						id="searchBox"
+					/>
+				</span>
+			</ul>
+		</span>
+	)
+}
+
+export default CardsList
