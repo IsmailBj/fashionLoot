66fde71d5fe6d665006adafa4b97d4d1a0334889
omer0x <omermusliu@gmail.com>
2023-12-07 11:01:50 +0100

    footer_update_icons1.0

--- app/src/components/Footer/FooterContainer.jsx
+++ app/src/components/Footer/FooterContainer.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import './Footer.css';
+import Fb from 'ionicons/dist/svg/logo-facebook.svg';
+import Insta from 'ionicons/dist/svg/logo-instagram.svg';
+import Twitter from 'ionicons/dist/svg/logo-twitter.svg';
+import Youtube from 'ionicons/dist/svg/logo-youtube.svg';
 
 const Footer = () => {
   return (
     <div className="footer-basic">
       <footer>
         <div className="social">
-          <a href="#"><i className="icon ion-social-instagram"></i></a>
-          <a href="#"><i className="icon ion-social-snapchat"></i></a>
-          <a href="#"><i className="icon ion-social-twitter"></i></a>
-          <a href="#"><i className="icon ion-social-facebook"></i></a>
+          <a href="#"><img src={Insta} alt="" className="icon-img" /><i className="icon ion-social-instagram"></i></a>
+          <a href="#"><img src={Youtube} alt="" className="icon-img" /><i className="icon ion-social-youtube"></i></a>
+          <a href="#"><img src={Twitter} alt="" className="icon-img" /><i className="icon ion-social-twitter"></i></a>
+          <a href="#"><img src={Fb} alt="" className="icon-img" /><i className="icon ion-social-facebook"></i></a>
         </div>
         <ul className="list-inline">
           <li className="list-inline-item"><a href="#">Home</a></li>
